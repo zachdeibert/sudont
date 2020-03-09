ad96bab750e5ac73b0648191cac9389d2a37fe93
Zach Deibert <zachdeibert@gmail.com>
2020-03-09 17:09:16 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# sudont
+
+Does what `sudo` does, but runs the command as your user instead of as root.
---
+++ src/main.c
@@ -0,0 +1,9 @@
+#include <config.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, const char **argv) {
+    puts("Hello, world!");
+    puts("This is " PACKAGE_STRING ".");
+    return EXIT_SUCCESS;
+}
